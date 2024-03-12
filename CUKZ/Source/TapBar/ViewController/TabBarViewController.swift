23cc50fe52aeaf10a55d42815a3eb5b0ac6fb44c
moichi <zzikbu@gmail.com>
2024-03-13 01:47:48 +0900

    [#10] 찜 페이지 탭바 구현

--- CUKZ/Source/TapBar/ViewController/TabBarViewController.swift
+++ CUKZ/Source/TapBar/ViewController/TabBarViewController.swift
@@ -12,6 +12,32 @@ final class TabBarViewController: UITabBarController {
     // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .gadaeGray
+        self.selectedIndex = 0
+        
+        tabBarConfig()
+    }
+    
+    private func tabBarConfig() {
+        tabBar.barTintColor = .gadaeGray
+        tabBar.tintColor = .white
+        tabBar.isTranslucent = false
+        
+        let likeVC = UINavigationController(rootViewController: LikeViewController())
+        
+        // 탭 바 이이템 설정
+        likeVC.tabBarItem = UITabBarItem(
+            title: "좋아요",
+            image: UIImage(systemName: "bookmark"),
+            selectedImage: UIImage(systemName: "bookmark.fill")
+        )
+        
+        let tabItems = [
+            likeVC
+        ]
+        
+        setViewControllers(tabItems, animated: true)
     }
     
 }
