bda3c29137f7d3efedfe5b0fa1482a803f7ca939
moichi <zzikbu@gmail.com>
2024-04-09 15:52:35 +0900

    [#45] Feat: 회원가입 2차 비밀번호 추가 및 키보드 처리

--- CUKZ/Source/UserAuthentication/View/SignUpView.swift
+++ CUKZ/Source/UserAuthentication/View/SignUpView.swift
@@ -9,9 +9,11 @@ import UIKit
 
 final class SignUpView: UIView {
     // MARK: - View
-    let scrollView = UIScrollView()
+    let scrollView = UIScrollView().then {
+        $0.keyboardDismissMode = .onDrag // 스크롤 시 키보드 숨김
+    }
     
-    private let contentView = UIView()
+    let contentView = UIView()
     
     // 아이디
     private let idLabel = UILabel().then {
@@ -65,6 +67,33 @@ final class SignUpView: UIView {
         $0.isSecureTextEntry = true
     }
     
+    // 공백뷰
+    private let spacer1 = UIView()
+    
+    // 2차 비밀번호
+    private let secondPasswordLabel = UILabel().then() {
+        $0.text = "비밀번호 재설정을 위한 2차 비밀번호"
+        $0.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+    private let secondPasswordRoundView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 5
+        $0.layer.borderWidth = 1
+    }
+    
+    let secondPasswordTextField = UITextField().then {
+        $0.placeholder = "2차 비밀번호를 입력해주세요"
+        $0.backgroundColor = .clear
+        $0.autocapitalizationType = .none
+        $0.autocorrectionType = .no
+        $0.spellCheckingType = .no
+        $0.isSecureTextEntry = true
+    }
+    
+    // 공백뷰
+    private let spacer2 = UIView()
+    
     // 닉네임
     private let nicknameLabel = UILabel().then() {
         $0.text = "닉네임"
@@ -85,8 +114,13 @@ final class SignUpView: UIView {
         $0.spellCheckingType = .no
     }
     
+    // 스택뷰
+    let stackView = UIStackView().then {
+        $0.axis = .vertical
+    }
+    
     // 가입하기 버튼
-    let signUpButton = UIButton().then {
+    let completeButton = UIButton().then {
         $0.setTitle("가입하기", for: .normal)
         $0.backgroundColor = .lightGray
         $0.isEnabled = false
@@ -107,29 +141,43 @@ final class SignUpView: UIView {
     
     // MARK: - UI
     private func addViews() {
-        self.addSubviews([scrollView, signUpButton])
+        self.addSubviews([
+            scrollView,
+            completeButton
+        ])
         
-        scrollView.addSubviews([contentView])
+        scrollView.addSubview(contentView)
         
-        contentView.addSubviews([idLabel,
-                                 idRoundView,
-                                 duplicateCheckLabel,
-                                 duplicateCheckButton,
-                                 passwordLabel,
-                                 passwordRoundView,
-                                 nicknameLabel,
-                                 nicknameRoundView])
+        contentView.addSubviews([
+            idLabel,
+            idRoundView,
+            duplicateCheckLabel,
+            duplicateCheckButton,
+            stackView
+        ])
         
-        idRoundView.addSubviews([idTextField])
-        passwordRoundView.addSubviews([passwordTextField])
-        nicknameRoundView.addSubviews([nicknameTextField])
+        idRoundView.addSubview(idTextField)
+        passwordRoundView.addSubview(passwordTextField)
+        secondPasswordRoundView.addSubview(secondPasswordTextField)
+        nicknameRoundView.addSubview(nicknameTextField)
+        
+        stackView.addArrangedSubviews([
+            passwordLabel,
+            passwordRoundView,
+            spacer1,
+            secondPasswordLabel,
+            secondPasswordRoundView,
+            spacer2,
+            nicknameLabel,
+            nicknameRoundView
+        ])
     }
     
     private func configureConstraints() {
         scrollView.snp.makeConstraints { make in
             make.top.equalTo(self.safeAreaLayoutGuide)
             make.leading.trailing.equalToSuperview()
-            make.bottom.equalTo(signUpButton.snp.top)
+            make.bottom.equalTo(completeButton.snp.top)
         }
         
         contentView.snp.makeConstraints { make in
@@ -137,6 +185,7 @@ final class SignUpView: UIView {
             make.edges.equalTo(scrollView)
         }
         
+        // 아이디
         idLabel.snp.makeConstraints { make in
             make.top.equalTo(contentView).offset(20)
             make.leading.trailing.equalTo(contentView).inset(20)
@@ -152,6 +201,7 @@ final class SignUpView: UIView {
             make.edges.equalTo(idRoundView).inset(5)
         }
         
+        // 중복체크
         duplicateCheckLabel.snp.makeConstraints { make in
             make.top.equalTo(idRoundView.snp.bottom).offset(5)
             make.leading.equalTo(idLabel)
@@ -164,14 +214,15 @@ final class SignUpView: UIView {
             make.width.equalTo(80)
         }
         
-        passwordLabel.snp.makeConstraints { make in
+        // 스택뷰
+        stackView.snp.makeConstraints { make in
             make.top.equalTo(duplicateCheckLabel.snp.bottom).offset(20)
             make.leading.trailing.equalTo(idLabel)
+            make.bottom.equalTo(contentView)
         }
         
+        // 비밀번호
         passwordRoundView.snp.makeConstraints { make in
-            make.top.equalTo(passwordLabel.snp.bottom)
-            make.leading.trailing.equalTo(idLabel)
             make.height.equalTo(50)
         }
         
@@ -179,23 +230,36 @@ final class SignUpView: UIView {
             make.edges.equalTo(passwordRoundView).inset(5)
         }
         
-        nicknameLabel.snp.makeConstraints { make in
-            make.top.equalTo(passwordRoundView.snp.bottom).offset(20)
-            make.leading.trailing.equalTo(idLabel)
+        // 구분 뷰
+        spacer1.snp.makeConstraints { make in
+            make.height.equalTo(20)
         }
         
-        nicknameRoundView.snp.makeConstraints { make in
-            make.top.equalTo(nicknameLabel.snp.bottom)
-            make.leading.trailing.equalTo(idLabel)
+        // 2차 비밀번호
+        secondPasswordRoundView.snp.makeConstraints { make in
             make.height.equalTo(50)
-            make.bottom.equalTo(contentView)
+        }
+        
+        secondPasswordTextField.snp.makeConstraints { make in
+            make.edges.equalTo(secondPasswordRoundView).inset(5)
+        }
+        
+        // 구분 뷰
+        spacer2.snp.makeConstraints { make in
+            make.height.equalTo(20)
+        }
+        
+        // 닉네임
+        nicknameRoundView.snp.makeConstraints { make in
+            make.height.equalTo(50)
         }
         
         nicknameTextField.snp.makeConstraints { make in
             make.edges.equalTo(nicknameRoundView).inset(5)
         }
         
-        signUpButton.snp.makeConstraints { make in
+        // 가입하기 버튼
+        completeButton.snp.makeConstraints { make in
             make.leading.trailing.bottom.equalToSuperview()
             make.height.equalTo(80)
         }
--- CUKZ/Source/UserAuthentication/Viewcontroller/SignUpViewController.swift
+++ CUKZ/Source/UserAuthentication/Viewcontroller/SignUpViewController.swift
@@ -22,6 +22,9 @@ final class SignUpViewController: UIViewController {
         
         prepare()
         setupNaviBar()
+        setupKeyboardNotification()
+        setupTextField()
+        setupButton()
     }
     
     private func prepare() {
@@ -33,5 +36,95 @@ final class SignUpViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.barTintColor = .white
         navigationController?.navigationBar.tintColor = .black
+    }
+    
+    private func setupKeyboardNotification() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    private func setupTextField() {
+        signUpView.idTextField.delegate = self
+        signUpView.passwordTextField.delegate = self
+        signUpView.secondPasswordTextField.delegate = self
+        signUpView.nicknameTextField.delegate = self
+    }
+    
+    private func setupButton() {
+        signUpView.duplicateCheckButton.addTarget(
+            self,
+            action: #selector(duplicateCheckButtonTapped),
+            for: .touchUpInside
+        )
+        signUpView.completeButton.addTarget(
+            self,
+            action: #selector(completeButtonButtonTapped),
+            for: .touchUpInside
+        )
+    }
+}
+
+// MARK: - @objc
+extension SignUpViewController {
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let keyboardHeight = keyboardFrame.height
+        
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+        signUpView.scrollView.contentInset = contentInsets
+        signUpView.scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect = view.frame
+        aRect.size.height -= keyboardHeight
+        
+        if let activeTextField = findActiveTextField(in: signUpView.contentView) {
+            if !aRect.contains(activeTextField.frame.origin) {
+                signUpView.scrollView.scrollRectToVisible(activeTextField.frame, animated: true)
+            }
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        let contentInsets = UIEdgeInsets.zero
+        signUpView.scrollView.contentInset = contentInsets
+        signUpView.scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    private func findActiveTextField(in view: UIView) -> UITextField? {
+        for subview in view.subviews {
+            if let textField = subview as? UITextField, textField.isFirstResponder {
+                return textField
+            } else if let foundTextField = findActiveTextField(in: subview) {
+                return foundTextField
+            }
+        }
+        return nil
+    }
+    
+    @objc private func duplicateCheckButtonTapped() {
+        print("중복체크 버튼 눌림")
+    }
+    
+    @objc private func completeButtonButtonTapped() {
+        print("회원가입 버튼 눌림")
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension SignUpViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
