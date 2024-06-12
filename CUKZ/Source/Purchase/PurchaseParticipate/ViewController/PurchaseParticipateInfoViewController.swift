180a85680ef363f0370c4d745be651ab5cf62a33
moichi <zzikbu@gmail.com>
2024-06-12 20:10:57 +0900

    Feat: 구매하기 API 연결

--- CUKZ/Source/Purchase/PurchaseParticipate/ViewController/PurchaseParticipateInfoViewController.swift
+++ CUKZ/Source/Purchase/PurchaseParticipate/ViewController/PurchaseParticipateInfoViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 final class PurchaseParticipateInfoViewController: UIViewController {
     // MARK: - Properties
-    var optionList: [ProductDetailModel.Option]? // 상세보기에서 넘어온 옵션 배열
+    var requestOptionList: [PurchaseParticipateRequest.OptionList]? // 옵션선택에서 넘어온 옵션 배열
+    var productId: Int?
     
     private let purchaseParticipateInfoView = PurchaseParticipateInfoView()
     
@@ -41,98 +42,40 @@ final class PurchaseParticipateInfoViewController: UIViewController {
 extension PurchaseParticipateInfoViewController {
     // 구매하기 버튼
     @objc private func completeButtonTapped() {
-//        guard let topView = purchaseParticipateView.tableView.tableHeaderView as? PurchaseParticipateTopView else {
-//            print(purchaseParticipateView.tableView.tableHeaderView as? PurchaseParticipateTopView)
-//            return
-//        }
-//
-//        guard let productId = self.productId else { return }
-//
-//        guard let buyerName = topView.buyerNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !buyerName.isEmpty else {
-//            showAlertWithDismissDelay(message: "구매자 이름을 입력해주세요.")
-//            return
-//        }
-//
-//        guard let buyerPhone = topView.buyerPhoneTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !buyerPhone.isEmpty else {
-//            showAlertWithDismissDelay(message: "구매자 전화번호를 입력해주세요.")
-//            return
-//        }
-//
-//        guard let recipientName = topView.recipientNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !recipientName.isEmpty else {
-//            showAlertWithDismissDelay(message: "수령인 이름을 입력해주세요.")
-//            return
-//        }
-//
-//        guard let recipientPhone = topView.recipientPhoneTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !recipientPhone.isEmpty else {
-//            showAlertWithDismissDelay(message: "수령인 전화번호를 입력해주세요.")
-//            return
-//        }
-//
-//        guard let address = topView.addressTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !address.isEmpty else {
-//            showAlertWithDismissDelay(message: "주소를 입력해주세요.")
-//            return
-//        }
-//
-//        guard let payerName = topView.payerNameTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines), !payerName.isEmpty else {
-//            showAlertWithDismissDelay(message: "입금자 이름을 입력해주세요.")
-//            return
-//        }
-//
-//        guard let refundName = topView.refundNameTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines), !refundName.isEmpty else {
-//            showAlertWithDismissDelay(message: "환불 계좌주 이름을 입력해주세요.")
-//            return
-//        }
-//
-//        guard let refundAccount = topView.refundAccountTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines), !refundAccount.isEmpty else {
-//            showAlertWithDismissDelay(message: "환불 계좌를 입력해주세요.")
-//            return
-//        }
-//
-//        // 옵션 데이터 수집
-//        var requestOptionList: [PurchaseParticipateRequest.OptionList] = []
-//        var hasValidQuantity = false
-//
-//        for index in 0..<purchaseParticipateView.tableView.numberOfRows(inSection: 0) {
-//            guard let cell = purchaseParticipateView.tableView.cellForRow(at: IndexPath(row: index, section: 0)) as? DemandParticipateCell,
-//                  let optionId = optionList?[index].id,
-//                  let quantityText = cell.quantityTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
-//                  !quantityText.isEmpty,
-//                  let quantity = Int(quantityText), quantity > 0 else {
-//                continue
-//            }
-//
-//            let option = PurchaseParticipateRequest.OptionList(optionId: optionId, quantity: quantity)
-//            requestOptionList.append(option)
-//            hasValidQuantity = true
-//        }
-//
-//        if !hasValidQuantity {
-//            showAlertWithDismissDelay(message: "수량을 정확히 입력해주세요.")
-//            return
-//        }
-//
-//        let parameters = PurchaseParticipateRequest(
-//            buyerName: buyerName,
-//            buyerPhone: buyerPhone,
-//            recipientName: recipientName,
-//            recipientPhone: recipientPhone,
-//            address: address,
-//            payerName: payerName,
-//            refundName: refundName,
-//            refundAccount: refundAccount,
-//            optionList: requestOptionList
-//        )
-//
-//        PurchaseNetworkManager.shared.postPurchase(productId: productId, parameters: parameters) { error in
-//            if let error = error {
-//                print("구매 하기 실패 - \(error.localizedDescription)")
-//            } else {
-//                print("구매 하기 성공")
-//                self.showAlertWithDismissDelay(message: "구매 완료")
-//                DispatchQueue.main.async {
-//                    self.navigationController?.popViewController(animated: true)
-//                }
-//            }
-//        }
+        guard let productId = self.productId,
+              let requestOptionList = self.requestOptionList else { return }
+        
+        guard let buyerName = self.purchaseParticipateInfoView.buyerNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !buyerName.isEmpty,
+              let buyerPhone = self.purchaseParticipateInfoView.buyerPhoneTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !buyerPhone.isEmpty,
+              let recipientName = self.purchaseParticipateInfoView.recipientNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !recipientName.isEmpty,
+              let recipientPhone = self.purchaseParticipateInfoView.recipientPhoneTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !recipientPhone.isEmpty,
+              let address = self.purchaseParticipateInfoView.addressTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !address.isEmpty,
+              let payerName = self.purchaseParticipateInfoView.payerNameTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines), !payerName.isEmpty,
+              let refundName = self.purchaseParticipateInfoView.refundNameTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines), !refundName.isEmpty,
+              let refundAccount = self.purchaseParticipateInfoView.refundAccountTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines), !refundAccount.isEmpty else {
+            showAlertWithDismissDelay(message: "모두 입력해주세요")
+            return
+        }
+        
+        let parameters = PurchaseParticipateRequest(buyerName: buyerName, buyerPhone: buyerPhone, recipientName: recipientName, recipientPhone: recipientPhone, address: address, payerName: payerName, refundName: refundName, refundAccount: refundAccount, optionList: requestOptionList)
+        
+        PurchaseNetworkManager.shared.postPurchase(productId: productId, parameters: parameters) { error in
+            if let error = error {
+                print("구매하기 실패: \(error.localizedDescription)")
+            } else {
+                print("구매하기 성공")
+                DispatchQueue.main.async {
+                    if let navigationController = self.navigationController {
+                        for viewController in navigationController.viewControllers {
+                            if viewController is ProductDetailViewController {
+                                navigationController.popToViewController(viewController, animated: true)
+                                break
+                            }
+                        }
+                    }
+                    self.showAlertWithDismissDelay(message: "구매하기 참여 완료")
+                }
+            }
+        }
     }
 }
--- CUKZ/Source/Purchase/PurchaseParticipate/ViewController/PurchaseParticipateOptionViewController.swift
+++ CUKZ/Source/Purchase/PurchaseParticipate/ViewController/PurchaseParticipateOptionViewController.swift
@@ -12,11 +12,11 @@ final class PurchaseParticipateOptionViewController: UIViewController {
     var optionList: [ProductDetailModel.Option]? // 상세보기에서 넘어온 옵션 배열
     var productId: Int?
     
-    private let purchaseParticipateView = PurchaseParticipateOptionView()
+    private let purchaseParticipateOptionView = PurchaseParticipateOptionView()
     
     // MARK: - View 설정
     override func loadView() {
-        view = purchaseParticipateView
+        view = purchaseParticipateOptionView
     }
     
     // MARK: - ViewDidLoad
@@ -34,14 +34,14 @@ final class PurchaseParticipateOptionViewController: UIViewController {
     }
     
     private func setupTableView() {
-        let tb = purchaseParticipateView.tableView
+        let tb = purchaseParticipateOptionView.tableView
         tb.dataSource = self
         tb.rowHeight = 55
         tb.register(DemandParticipateCell.self, forCellReuseIdentifier: "DemandParticipateCell")
     }
     
     private func setupButton() {
-        purchaseParticipateView.completeButton.addTarget(self,
+        purchaseParticipateOptionView.completeButton.addTarget(self,
                                                          action: #selector(completeButtonTapped),
                                                          for: .touchUpInside)
     }
@@ -49,9 +49,36 @@ final class PurchaseParticipateOptionViewController: UIViewController {
 
 // MARK: - Actions
 extension PurchaseParticipateOptionViewController {
-    
+    // 개입정보 입력 버튼
     @objc private func completeButtonTapped() {
+        guard let productId = self.productId else { return }
+        
+        // 테이블 뷰에서 각 셀의 데이터를 수집하여 `requestOptionList` 배열에 저장
+        var requestOptionList: [PurchaseParticipateRequest.OptionList] = []
+        var hasValidQuantity = false
+
+        for index in 0..<purchaseParticipateOptionView.tableView.numberOfRows(inSection: 0) {
+            guard let cell = purchaseParticipateOptionView.tableView.cellForRow(at: IndexPath(row: index, section: 0)) as? DemandParticipateCell,
+                  let optionId = optionList?[index].id,
+                  let quantityText = cell.quantityTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+                  !quantityText.isEmpty,
+                  let quantity = Int(quantityText), quantity > 0 else {
+                continue
+            }
+
+            let option = PurchaseParticipateRequest.OptionList(optionId: optionId, quantity: quantity)
+            requestOptionList.append(option)
+            hasValidQuantity = true
+        }
+
+        if !hasValidQuantity {
+            showAlertWithDismissDelay(message: "수량을 정확히 입력해주세요.")
+            return
+        }
+        
         let VC = PurchaseParticipateInfoViewController()
+        VC.productId = productId
+        VC.requestOptionList = requestOptionList
         navigationController?.pushViewController(VC, animated: true)
     }
 }
